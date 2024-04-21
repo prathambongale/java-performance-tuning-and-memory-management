7ad91f0d77474eee7b679021b4bb74d94dac7aa5
prathambongale <prathambongale@hotmail.com>
2024-04-21 22:54:14 +0200

    First example for tunning

---
+++ first-example/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        Integer max = Integer.parseInt(args[0]);
+        primeNumbers.generateNumbers(max);
+    }
+}
