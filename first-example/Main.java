1cfa618812088e1e7f5bcb9ec1a7147765ad2fab
prathambongale <prathambongale@hotmail.com>
2024-04-22 08:13:50 +0200

    first example modified

--- first-example/Main.java
+++ first-example/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(20000);
         PrimeNumbers primeNumbers = new PrimeNumbers();
         Integer max = Integer.parseInt(args[0]);
         primeNumbers.generateNumbers(max);
